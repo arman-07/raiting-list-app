ac56300bff98296312c584c84fb6582fc1adda60
asmartiros <asmartiros@mts.ru>
2023-05-13 12:47:57 +0300

    Получение данных с бэка

---
+++ src/services/userApi.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+import { User } from '../types/index'
+
+export async function getUsers(): Promise<User[]> {
+  try {
+    const response = await axios.get("https://random-data-api.com/api/users/random_user?size=3");
+    console.log(response.data);
+    
+    return response.data.map((user: any) => ({
+      id: user.id,
+      name: user.username,
+    }));
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
